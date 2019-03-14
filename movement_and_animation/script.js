0828409d020516efaa71d13dd9e4467e87d91f4c
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-03-14 14:28:52 -0400

    Update script.js

--- movement_and_animation/script.js
+++ movement_and_animation/script.js
@@ -1,12 +1,19 @@
+//directional variables
 var goRight=false;
 var goLeft=false;
 var goUp=false;
 var goDown=false;
+//var for whether or not character is moving
+var moving = false;
+//tell computer what keydown and keyup mean
 document.addEventListener('keydown', keyPressed, false);
 document.addEventListener('keyup', keyUnpressed, false);
+
+
+//create canvas
 //Width and height for our canvas
-var canvasWidth = 96;
-var canvasHeight = 96;
+var canvasWidth = 420;
+var canvasHeight = 420;
 //Getting the canvas
 var canvas = document.getElementById('canvas');
 //setting width and height of the canvas
@@ -14,46 +21,64 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 //Establishing a context to the canvas
 var context = canvas.getContext("2d");
-//Creating an Image object for our character
-var maincharacter = new Image();
-//Setting the source to the image file
-maincharacter.src = "playersprite.png";
+function background(){
+  document.body.style.background = "backgroundimage.png";
+}
 
+
+//importing graphics
+var maincharacterImage = new Image();
+maincharacterImage.src = "playersprite.png";
+
+//declare sprite
 function sprite(options){
-
   var that = {};
 
   // values for this case
-  //that.context=options.context
   that.x=options.x;
   that.y=options.y;
   that.srcX=options.srcX;
   that.srcY=options.srcY;
   that.lastX=options.lastX;
   that.lastY=options.lastY;
-  that.width=options.width;
-  that.height=options.height;
+  that.sheetwidth=options.sheetwidth;
+  that.sheetheight=options.sheetheight;
+  that.sizewidth=options.sizewidth;
+  that.sizeheight=options.sizeheight;
   that.image=options.image;
   that.frameCount=options.frameCount;
   that.curFrame=options.curFrame;
 
+  //SHOW FUNCTION
   that.show=function()
   {
-      context.drawImage(that.image,that.srcX, that.srcY,that.width,that.height,that.x,that.y,that.width,that.height);
+    //draw image
+      context.drawImage(that.image,that.srcX, that.srcY,that.sheetwidth,that.sheetheight,that.x,that.y,that.sizewidth,that.sizeheight);
   }
 
+  //UPDATE FUNCTION
   that.update=function(){
-   //Updating the frame index
-   that.curFrame = ++that.curFrame % that.frameCount;
+  //player moving frame updates
+    if(moving){
+      //Updating the frame index
+      that.curFrame = ++that.curFrame % that.frameCount;
+    }
+    //if not moving current frame is 0
+    else {
+      //set current frame to 0
+        that.curFrame = 0;
 
-   //Calculating the x coordinate for spritesheet
-   that.srcX = that.curFrame * that.width;
-  		context.clearRect(that.lastX,that.lastY,that.width,that.height);
+    }
+    //Calculating the x coordinate for spritesheet
+    that.srcX = that.curFrame * that.sheetwidth;
+    //clear
+       context.clearRect(that.lastX,that.lastY,that.sizewidth,that.sizeheight);
   }
 
   return that;
 }
 
+//create a sprite for the player
 var player = sprite({
   //context: canvas.getContext("2d"),
   x: 30,
@@ -62,16 +87,35 @@ var player = sprite({
   srcY: 0,
   lastX:0,
   lastY:0,
-  width: 32,
-  height: 32,
-  image: maincharacter,
+  sheetwidth: 32,
+  sheetheight: 32,
+  sizewidth: 96,
+  sizeheight: 96,
+  image: maincharacterImage,
   frameCount: 3,
   curFrame: 0
 })
 
+//declare obstacle
+function obstacle (x,y,width,height){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height  =  height;
 
 
+  this.show = function(){
+    fill(0);
+      rect(this.x, this.y, this.width, this.height);
+    }
+}
+
+//create a platform (walls)
+
+//gameloop
 function draw(){
+  //set background
+  background();
  //Updating the frame
 player.update();
  //Drawing the image
@@ -96,7 +140,6 @@ else if(goDown) {
 	 //character.y += 5;
 	 player.y+=5;
  }
- console.log("draw loop");
 }
 
 //for when the arrow keys are pressed
@@ -104,42 +147,50 @@ function keyPressed(event){
 	if (event.keyCode == '39')
 	{
 		goRight=true;
+      moving = true;
 	}
 	else if (event.keyCode == '37')
 	{
 		goLeft= true;
+      moving = true;
 	}
 	if (event.keyCode == '40')
 	{
 		goDown= true;
+      moving = true;
 	}
 	else if (event.keyCode == '38')
 	{
 		goUp= true;
+      moving = true;
 	}
+
 }
 //for when the keys are let go
 function keyUnpressed(event){
 	if (event.keyCode == '39')
 	{
 		goRight=false;
-
+    moving = false;
 	}
 	else if (event.keyCode == '37')
 	{
 		goLeft= false;
-
+    moving = false;
 	}
 	if (event. keyCode == '40')
 	{
 		goDown= false;
-
+    moving = false;
 	}
 	else if (event. keyCode == '38')
 	{
 		goUp= false;
-
+    moving = false;
 	}
-}
 
-setInterval(draw,200);
+}
+  //(for movement) if player is not vertically colliding, CAN vertically move, same apps. for horizontally
+
+//set for gameLoop to only occur every 100ms
+setInterval(draw,100);
